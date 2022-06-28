36fbfba4d1a875b73c53a91c18c47d18eaf5e430
Cristian Camilo <cristian.gutierrez@adidas.com>
2022-06-28 11:04:17 -0500

    First commit in stacks push methods

---
+++ stack.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+}
+
+let newStack = new Stack(0);
+newStack.push(1);
+console.log(newStack);
