0b379698c6a6589a34bf527717d56380232fa178
Cristian Camilo <cristian.gutierrez@adidas.com>
2022-06-28 12:00:57 -0500

    Queue structure constructure

--- stack.js
+++ stack.js
@@ -25,6 +25,15 @@ class Stack {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let temp = this.top;
+    this.top = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 let newStack = new Stack(0);
