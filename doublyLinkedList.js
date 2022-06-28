8b5b0ebb7748985b912d51079678271ae9e2317e
Cristian Camilo <cristian.gutierrez@adidas.com>
2022-06-28 09:03:25 -0500

    shift method doublyLinked list

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -29,6 +29,55 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+    }
+
+    this.length--;
+    return temp;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = temp.next;
+      temp.prev = null;
+      temp.next = null;
+    }
+
+    this.length--;
+    return temp;
+  }
 }
 
 let newDoublyLinkedList = new DoublyLinkedList(1);
