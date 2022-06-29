0d881e0ef4fd0d3c97643b3ae38e097b40e60f09
Cristian Camilo <cristian.gutierrez@adidas.com>
2022-06-29 08:40:54 -0500

    Dequeue method queue

--- queue.js
+++ queue.js
@@ -12,6 +12,44 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+  }
+
+  push(value) {
+    const myNode = new Node(value);
+
+    if (!this.first) {
+      this.first = myNode;
+      this.last = myNode;
+    } else {
+      this.last.next = myNode;
+      this.last = myNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) return undefined;
+    let temp = this.first;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
 
 let myQueue = new Queue(4);
