8851b7fed83d527a8e95d678b632c69882012326
Cristian Camilo <cristian.gutierrez@adidas.com>
2022-07-18 15:32:55 -0500

    Setting environment variables

--- node-course-next-mongo/4-natours/starter/app.js
+++ node-course-next-mongo/4-natours/starter/app.js
@@ -1,11 +1,14 @@
+const morgan = require('morgan');
 const express = require('express');
 // for comfort we stored it in a app varialbe
 const app = express();
 const { json } = require('express');
 // we can use another library called morgan which can provide more inf about the request in the cosnole
 // this can be very usefull and save us some time
-const morgan = require('morgan');
-app.use(morgan('dev'));
+// we can execute this libreary deppendign on the environment we are at by
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // impoprt our tours and users rout their modules
 const tourRouter = require('./routs/toursRout');
@@ -14,6 +17,12 @@ const userRouter = require('./routs/usersRout');
 // this is a step which the request has to go through.....
 // This is a middleware fucntion that is necessary in order to get access to the body in the post method.
 app.use(express.json());
+
+// ---------------- Accessing static files
+// if we type http...../overview.html in the browser it will render that file
+// The browser will set the path below as the root. Therefore we can access any file from the browser
+// so we can even access an image within that folder
+app.use(express.static(`${__dirname}/public`));
 
 // we can create our own middleware functions , as follow.
 app.use((req, res, next) => {
--- node-course-next-mongo/4-natours/starter/server.js
+++ node-course-next-mongo/4-natours/starter/server.js
@@ -1,7 +1,21 @@
-// This is our conection with app.js
+// Settind emv varialbles
+const dotenv = require('dotenv');
+//-----How to set environemetn valiables
+// This next line will provide access to the global environmet variables defined in teh config.env file
+dotenv.config({ path: './config.env' });
+const port = process.env.PORT;
 
+// This is our conection with app.js
+// on order to get access to the env variables wee need to call the line below just after setting our dotenv ...
 const app = require('./app');
-const port = 3000;
+
+//-----How ro get the environemetn valiables
+// By default express sets environment variables to  development
+// this varialbes are global variables taht are used to set the  to define the environment
+console.log(app.get('env'));
+
+// We can check those var console logging process.env
+//console.log(process.env);
 
 app.listen(port, () => {
   console.log(`The serves has started at port ${port}`);
